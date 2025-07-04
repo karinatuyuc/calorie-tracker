af91eca8fff4b964b74d32fad0d9a107cdf3b8f6
Karina <karina8tuyuc@gmail.com>
2025-07-04 16:20:30 -0600

    Creating the header and form part

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+function App(){
+  return(
+    <>
+    {/*Header y boton */}
+    <header className="bg-lime-600 py-3">
+      <div className="max-w-4xl mx-auto flex justify-between">
+        <h1 className="text-center text-lg font-bold text-white uppercase">
+          Contador de Calorias
+        </h1>
+      </div>
+    </header>
+
+    {/*Seccion del formulario*/}
+    <section className="bg-lime-500 py-20 px-5">
+      <div className="max-w-4xl mx-auto">
+        <p>Formulario aqui</p>
+      </div>
+    </section>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App 
