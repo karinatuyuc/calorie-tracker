1029d86259f2d3e01f16e356a56646d29add09fb
Karina <karina8tuyuc@gmail.com>
2025-07-09 22:59:02 -0600

    Creating the state for the activities

--- src/componentes/Form.tsx
+++ src/componentes/Form.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 
 export default function Form(){
+
+    //Definiendo el state para las categorias, activiades y calorias en un ojeto
+    const [activity, setActivity] = useState({
+        category: 2,
+        name: '',
+        calories: 300
+    })
+
 
     //Sin librerias
     return(
@@ -13,6 +22,7 @@ export default function Form(){
                 <select
                   className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                   id="category"
+                  value={activity.category}
                 >
                     {/*Iterando en la base de datos*/}
                     {categories.map(category => (
@@ -29,13 +39,14 @@ export default function Form(){
 
             {/*Campo para ingresar la actividad */}
             <div className="grid grid-cols-1 gap-3 ">
-                <label htmlFor="activity" className="font-bold">Actividad:</label>
+                <label htmlFor="name" className="font-bold">Actividad:</label>
                 {/*el id="activity" sirve para activar el input de cada label*/}
                 <input 
-                 id="activity" 
+                 id="name" 
                  type="text"
                  className="border border-slate-300 p-2 rounded-lg"
                  placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+                 value={activity.name}
                 />
             </div>
 
@@ -47,6 +58,7 @@ export default function Form(){
                 type="number"
                 className="border border-slate-300 rounded-lg p-2"
                 placeholder="Calorias, ej. 300 o 500"
+                value={activity.calories}
                 />
             </div>
 
