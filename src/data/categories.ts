942a92978feae4df9258fd1f8a673861ec17bbcd
Karina <karina8tuyuc@gmail.com>
2025-07-14 22:38:09 -0600

    Nothing

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,5 +3,11 @@ import type { Category } from "../types"
 
 export const categories: Category[] = [
     { id: 1, name: 'Comida'},
-    { id: 2, name: 'Ejercicio'}
+    { id: 2, name: 'Ejercicio'} //It's not recognized the id, but let's see if does now
 ]
+
+export type Activity = {
+    category: number,
+    name: string,
+    calories: number
+}
