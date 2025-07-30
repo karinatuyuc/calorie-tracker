c67f8adb2358cda2ca631e40b47bcdf9b635cea8
Karina <karina8tuyuc@gmail.com>
2025-07-29 22:12:05 -0600

    Creando el hook useReducer

---
+++ src/reducers/activity-reducers.ts
@@ -0,0 +1,21 @@
+
+//Type que describe que va a pasar en el reducer de la actividad
+export type ActivityAction = {
+
+}
+
+//Type para describit el estado inical 
+type ActivityState = {
+
+}
+
+
+
+export const initialState = {
+
+}
+
+export const activityReducer = () => {
+
+}
+
