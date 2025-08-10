e3769cd717f728923dcad679cd8f00dcccb2090c
Karina <karina8tuyuc@gmail.com>
2025-08-09 22:13:54 -0600

    09/08

--- src/reducers/activity-reducers.ts
+++ src/reducers/activity-reducers.ts
@@ -1,4 +1,5 @@
 import type { Activity } from "../types"
+import { ActivityAction } from "../actions/activity-actions";
 
 //Type que describe que va a pasar en el reducer de la actividad
 export type ActivityAction = 
@@ -26,14 +27,20 @@ export const activityReducer = (
         if(action.type === 'save-activity'){
             //Este codigo maneja la logica para actualizar el state}
 
+            console.log("Se ha guardado una nueva actividad", action.payload.newAcrivity);
+            //testeando 
+
             //Retorna un nuevo estado con la nueva actividad añadida
             return {
-                ...state,
-                activities: [...state.activities, action.payload.newAcrivity]
+                ...state,//Copiamos el estado que teniamos para que no se pierdan los datos
+                activities: [...state.activities, action.payload.newAcrivity ] //Agregamos la nueva actividad al array de actividades
+
 
             }
 
 
     }
-    return state; //Esta linea es fundamental si se cumple o no, siempre retorna el estado actual
+
+    return state;
+    //Esta linea es fundamental si se cumple o no, siempre se retorna el estado actual
 }
