830206527fa2268e43ce605c6118ee7330ea6e78
Karina <karina8tuyuc@gmail.com>
2025-08-12 23:02:04 -0600

    Creanod el componente activityList

---
+++ src/componentes/ActivityList.tsx
@@ -0,0 +1,12 @@
+
+
+export default function ActivityList(){
+
+    return (
+        <>
+
+         <h2 className="text-4xl font-bold text-slate-600 text-center">Comida y Activiades</h2>
+        
+        </>
+    )
+}
