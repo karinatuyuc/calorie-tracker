d0510cfb24feb8fd6a3e1f60521722bae9d417ae
Karina <karina8tuyuc@gmail.com>
2025-08-13 17:15:28 -0600

    Almacenando el Id a editar

--- src/componentes/ActivityList.tsx
+++ src/componentes/ActivityList.tsx
@@ -1,14 +1,17 @@
+import { useMemo } from "react"
+import type { Dispatch } from 'react'
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
-import { useMemo } from "react"
 import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import type { ActivityAction } from "../reducers/activity-reducers"
 
 type ActivityListProps = {
     activities: Activity[];
+    dispatch: Dispatch<ActivityAction>
 }
 
 
-export default function ActivityList({activities}: ActivityListProps) {
+export default function ActivityList({activities, dispatch}: ActivityListProps) {
 
     const categoryName = useMemo(() => 
         (category : Activity['category']) => categories.map( cat => cat.id === category ? cat.name : ''), 
@@ -41,7 +44,9 @@ export default function ActivityList({activities}: ActivityListProps) {
                 </div>
 
                 <div className="flex gap-5 items-center">
-                    <button>
+                    <button
+                       onClick={() => dispatch({type: "set-activeId", payload: {id: activity.id} })}
+                    >
                         <PencilSquareIcon
                              className="h-8 w-8 text-gray-800"
                         />
