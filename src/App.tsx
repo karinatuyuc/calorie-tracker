06cb5a7b3657d9fc49cedc1d96a24b2803e0ebb1
Karina <karina8tuyuc@gmail.com>
2025-09-25 22:15:38 -0600

    Show total calories

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,14 @@ function App(){
       </div>
     </section>
 
+    <section className="bg-gray-800 py-10">
+      <div  className="max-w-4xl mx-auto">
+        <CalorieTracker
+          activities={state.activities}
+        />
+      </div>
+    </section>
+
     <section className="p-10 mx-auto max-w-4xl">
       <ActivityList 
        activities={state.activities}
