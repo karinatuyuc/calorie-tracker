62f8c880958baab658de64f1e3ef93d82ef67667
Karina <karina8tuyuc@gmail.com>
2025-10-02 23:22:42 -0600

    Showing calories burned

--- src/componentes/CalorieDisplay.tsx
+++ src/componentes/CalorieDisplay.tsx
@@ -1,17 +1,7 @@
 
-type CaloriesDisplayProps = {
-    calories: number
-    text: string
-}
 
 
-export default function CalorieDisplay({calories, text} : CaloriesDisplayProps) {
+export default function CalorieDisplay() {
     return (
-        <>
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-            <span  className="font-black text-6xl text-orange">{calories}</span>
-            {text}
-        </p>
-        </> //Este es el cierre del fragmente
     )
 }
--- src/componentes/CalorieTracker.tsx
+++ src/componentes/CalorieTracker.tsx
@@ -8,19 +8,29 @@ export default function CalorieTracker({activities} : CaloriteTrackerProps) {
 
     // Contadores de calorias
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => 
-        activity.category === 1 ? total + activity.calories : total, 0), [activities]);
+    activity.category === 1 ? total + activity.calories : total, 0), [activities]);
+
+    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => 
+    activity.category === 2 ? total + activity.calories : total, 0 ) , [activities]);
 
     return (
         <>
         <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
 
-        <div className="flex flex-col items-center md:flex-row md:justify-betwen gap-5 mt-10">
+        <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
             <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
 
                 <span className='font-black text-6xl text-orange'>
                     {caloriesConsumed}
                 </span>
                 Consumidas
+            </p>
+
+            <p className='text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center'>
+                <span className='font-black text-6xl text-red'>
+                    {caloriesBurned}
+                </span>
+                Ejercicio
             </p>
 
         </div>
